f64a0b7f43220e9392133609590ce71d64f8bf6f
TryAgainRoViLaDa <83392052+TryAgainRoViLaDa@users.noreply.github.com>
2021-05-29 19:57:09 +0200

    HEROKU2

--- web/conexion.php
+++ web/conexion.php
@@ -2,10 +2,10 @@
 
 function conectar()
 {
-	$user="root";
+	$user="id16927528_tryagainrodavila";
 	$pass="";
 	$server="localhost";
-	$db="tryagain";
+	$db="id16927528_tryagain";
 	$con=new mysqli($server, $user, $pass, $db) or die ("Error al conectar a la base de datos");
 
 	return $con;
