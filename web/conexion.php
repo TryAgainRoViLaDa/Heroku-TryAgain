52428814bace02209dcfd43463dc3452aec38baa
TryAgainRoViLaDa <83392052+TryAgainRoViLaDa@users.noreply.github.com>
2021-05-29 21:13:45 +0200

    HEROKU4

--- web/conexion.php
+++ web/conexion.php
@@ -2,11 +2,11 @@
 
 function conectar()
 {
-	$user="id16927528_tryagain_";
-	$pass="zlpF{3Lgj}AB%$0)";
-	$server="localhost";
-	$db="id16927528_tryagain";
-	$con=new mysqli($server, $user, $pass, $db) or die ("Error al conectar a la base de datos");
+	//$user="id16927528_tryagain_";
+	//$pass="zlpF{3Lgj}AB%$0)";
+	//$server="localhost";
+	//$db="id16927528_tryagain";
+	$con=new mysqli("localhost", "id16927528_tryagain_", "zlpF{3Lgj}AB%$0)", "id16927528_tryagain") or die ("Error al conectar a la base de datos");
 
 	return $con;
 }
